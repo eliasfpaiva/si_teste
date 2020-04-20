039696e4c22779e02f47902f15b2f381aecb2f87
Camila Couto <camilacoutodeoliveira@gmail.com>
2020-04-19 23:39:14 -0300

    validação e autentificação cpm API de teste

--- lib/models/Aluno.dart
+++ lib/models/Aluno.dart
@@ -2,14 +2,39 @@ import 'Turma.dart';
 import 'AreaConhecimento.dart';
 import 'Avatar.dart';
 
-class Aluno{
-  int id;
-  int matricula;
+class Aluno {
+  String id;
+  String matricula;
   String nome;
   Turma turma;
   Avatar avatar;
   AreaConhecimento areaConhecimento;
-}
-  
 
-  
+  Aluno(
+      {this.id,
+      this.matricula,
+      this.nome,
+      this.turma,
+      this.avatar,
+      this.areaConhecimento});
+
+  Aluno.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    matricula = json['matricula'];
+    nome = json['nome'];
+    turma = json['turma'];
+    avatar = json['avatar'];
+    areaConhecimento = json['areaConhecimento'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['matricula'] = this.matricula;
+    data['nome'] = this.nome;
+    data['turma'] = this.turma;
+    data['avatar'] = this.avatar;
+    data['areaConhecimento'] = this.areaConhecimento;
+    return data;
+  }
+}
---
+++ lib/services/login_api.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class LoginApi {
+  static Future<bool> login(String user, String password) async {
+    var url = 'https://www.macoratti.net.br/catalogo/api/contas/login';
+
+    var header = {"Content-Type": "application/json"};
+
+    Map params = {"username": user, "senha": password, "email": user};
+
+    var _body = json.encode(params);
+    print("json enviado : $_body");
+
+    var response = await http.post(url, headers: header, body: _body);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    Map mapResponse = json.decode(response.body);
+
+    String mensagem = mapResponse["message"];
+    String token = mapResponse["token"];
+
+    print("message $mensagem");
+    print("token $token");
+
+    return true;
+  }
+}
