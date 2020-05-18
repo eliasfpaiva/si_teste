a3494cbb046606fa8689d740e0da85894d6c91ca
Elias Paiva <eliaspaiva@irmaos.com>
2020-05-18 02:32:56 -0300

    Ajustes na tela de listagem de Unidades de Ensino - OS.:22

--- lib/views/views/tUnidades.dart
+++ lib/views/views/tUnidades.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:si_teste/Utils/valores.dart';
 import 'package:si_teste/models/UnidadeEnsino.dart';
 import 'package:si_teste/services/apiClient.dart';
 import 'package:si_teste/views/widgets/components.dart';
@@ -12,31 +13,48 @@ class TUnidades extends StatefulWidget {
 class _TUnidadesState extends State<TUnidades> {
   Future<List<UnidadeEnsino>> unidades = ApiClient.getListaUnidadesEnsino(1);
 
+  abrirAssuntos(int id) {
+    Valores.idUnidadeEnsionoSelecionada = id;
+    Navigator.pushNamed(context, Valores.rotaAssuntos);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Components.getDrawer(context),
       appBar: Components.getAppBar('Unidades'),
-      body: FutureBuilder<List<UnidadeEnsino>>(
-        future: unidades,
-        builder: (context, response){
-          if(response.hasData){
-            return ListView.builder(
-              itemCount: response.data.length,
-              itemBuilder: (BuildContext context, int index){
-                return CupertinoButton(
-                  child: Text(response.data[index].nome),
-                  onPressed : () => Navigator.pushNamed(context, "/Login/Principal/Unidades/Assuntos"),
-                );
-              },
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: FutureBuilder<List<UnidadeEnsino>>(
+          future: unidades,
+          builder: (context, response){
+            if(response.hasData){
+              return ListView.builder(
+                itemCount: response.data.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Container(
+                    padding: EdgeInsets.only(left: 5, top: 3, right: 5, bottom: 3),
+                    height: 50,
+                    child: MaterialButton(
+                      child: Text(response.data[index].nome, textAlign: TextAlign.center, maxLines: 2, style: TextStyle(fontSize: 15),),
+                      onPressed : () => abrirAssuntos(response.data[index].id),
+                      shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(10)),
+//                      color: Color(0xFF7f39fb),
+                      color: Color(Valores.getCor(index)),
+                      textColor: Colors.white,
+                      padding: EdgeInsets.all(2),
+                    ),
+                  );
+                },
+              );
+            }else if(response.hasError){
+              return Text("${response.error}");
+            }
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          }else if(response.hasError){
-            return Text("${response.error}");
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          },
+        ),
       ),
     );
   }
