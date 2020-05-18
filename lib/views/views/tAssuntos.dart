2302ddfdf307d7d84051501c3259bceb157b5550
Elias Paiva <eliaspaiva@irmaos.com>
2020-05-18 02:35:17 -0300

    Ajustes na tela de listagem de Assuntos - OS.:23

--- lib/views/views/tAssuntos.dart
+++ lib/views/views/tAssuntos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:si_teste/Utils/valores.dart';
 import 'package:si_teste/models/Assunto.dart';
 import 'package:si_teste/services/apiClient.dart';
 import 'package:si_teste/views/views/tQuiz.dart';
@@ -13,32 +14,72 @@ class TAssuntos extends StatefulWidget {
 class _TAssuntosState extends State<TAssuntos> {
   Future<List<Assunto>> assuntos = ApiClient.getListaAssuntos(1);
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    drawer: Components.getDrawer(context),
-    appBar: Components.getAppBar('Assuntos'),
-    body: FutureBuilder<List<Assunto>>(
-      future: assuntos,
-      builder: (context, response){
-        if(response.hasData){
-          return ListView.builder(
-            itemCount: response.data.length,
-            itemBuilder: (BuildContext context, int index){
-              return CupertinoButton(
-                child: Text(response.data[index].nome),
-                onPressed : () => Navigator.push(context, MaterialPageRoute(builder: (context) => TQuiz())),
+  abrirQuizz(int id) {
+    Valores.idAssuntoSelecionado = id;
+    Navigator.pushNamed(context, Valores.rotaQuizz);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      drawer: Components.getDrawer(context),
+      appBar: Components.getAppBar('Assuntos'),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: FutureBuilder<List<Assunto>>(
+          future: assuntos,
+          builder: (context, response){
+            if(response.hasData){
+              return ListView.builder(
+                itemCount: response.data.length,
+                itemBuilder: (BuildContext context, int index){
+                  return buildCardAssunto(response, index);
+                },
               );
-            },
-          );
-        }else if(response.hasError){
-          return Text("${response.error}");
-        }
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    ),
-  );
-}
+            }else if(response.hasError){
+              return Text("${response.error}");
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildCardAssunto(AsyncSnapshot<List<Assunto>> response, int index) {
+    return Container(
+      padding: EdgeInsets.only(left: 5, top: 3, right: 5, bottom: 3),
+      height: 50,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(child: Text(response.data[index].nome)),
+          MaterialButton(
+            child: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          MaterialButton(
+            child: Icon(Icons.live_help),
+            onPressed:() {} ,
+          ),
+        ],
+      ),
+    );
+
+//    return Container(
+//      padding: EdgeInsets.only(left: 5, top: 3, right: 5, bottom: 3),
+//      height: 50,
+//      child: MaterialButton(
+//        child: Text(response.data[index].nome, textAlign: TextAlign.center, maxLines: 2, style: TextStyle(fontSize: 15),),
+//        onPressed : () => abrirQuizz(response.data[index].id),
+//        shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(10)),
+//        color: Color(Valores.getCor(index)),
+//        textColor: Colors.white,
+//        padding: EdgeInsets.all(2),
+//      ),
+//    );
+  }
 }
